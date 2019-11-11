e3c093c40065d5f3edd8ccba99fd76c0de93b540
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2019-11-11 10:16:04 +0100

    Quiz and Answer

---
+++ CityLabGame/Assets/ChangeQuizText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeQuizText : MonoBehaviour
+{
+    string toChange = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text quizText = GameObject.Find("Canvas/Quiz Spørgsmål").GetComponent<Text>();
+        quizText.text = "Does this work";
+    }
+
+    //Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
