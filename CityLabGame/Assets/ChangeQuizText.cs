e47a6ff6ff3e1efd62f838966119219fb413bfa9
Robert4582 <43041342+Robert4582@users.noreply.github.com>
2019-11-11 10:29:10 +0100

    Text on answe button change

--- CityLabGame/Assets/ChangeQuizText.cs
+++ CityLabGame/Assets/ChangeQuizText.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 
 public class ChangeQuizText : MonoBehaviour
 {
-    string toChange = null;
-
     // Start is called before the first frame update
     void Start()
     {
         Text quizText = GameObject.Find("Canvas/Quiz Spørgsmål").GetComponent<Text>();
         quizText.text = "Does this work";
+        Text answerText1 = GameObject.Find("Canvas/Svar 1/Text").GetComponent<Text>();
+        answerText1.text = "Yes";
+        Text answerText2 = GameObject.Find("Canvas/Svar 2/Text").GetComponent<Text>();
+        answerText2.text = "No";
+        Text answerText3 = GameObject.Find("Canvas/Svar 3/Text").GetComponent<Text>();
+        answerText3.text = "Who knows?";
     }
 
     //Update is called once per frame
