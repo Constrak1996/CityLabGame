37799bdf2467fe17b56359ae0fc4dfb72e494a4f
Alex Frier Stewart <alex7175@student.eadania.dk>
2019-11-12 12:33:14 +0100

    i am a genis.. genius.. smart boy

---
+++ CityLabGame/Assets/Scripts/GameBoardStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameBoardStats : MonoBehaviour
+{
+    public static int redScore;
+    public static int blueScore;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
