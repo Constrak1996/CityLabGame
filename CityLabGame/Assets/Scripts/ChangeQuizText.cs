02c7a25836e0d04c0017e28b7d55be3ecee5b359
Peter Kryger Kristensen <glpekri8@student.eadania.dk>
2019-11-25 15:46:52 +0100

    Something

--- CityLabGame/Assets/Scripts/ChangeQuizText.cs
+++ CityLabGame/Assets/Scripts/ChangeQuizText.cs
@@ -10,89 +10,117 @@ public class ChangeQuizText : MonoBehaviour
     public bool answer = false;
     int number;
     private GameObject stall;
+    private List<string> questionList = new List<string>();
     [SerializeField]
     private GameObject quiz;
+    private Text quizText;
+    private Text answerText1;
+    private Button answer1;
+    private Text answerText2;
+    private Button answer2;
+    private Text answerText3;
+    private Button answer3;
     private Stall_Script stallScript;
+    private string question;
+    private string incorrect_one;
+    private string incorrect_two;
+    private string correct;
+    IDataReader reader;
+    string sqlQueryQuestion;
+    IDbCommand dbcmd;
+    IDbConnection dbconn;
 
     // Start is called before the first frame update
     void Start()
     {
+        number = Random.Range(0, 10);
         stall = GameObject.FindGameObjectWithTag("Stall");
         string conn = "URI=file:" + Application.dataPath + "/Database.s3db"; //Path to database.
-        IDbConnection dbconn;
         dbconn = (IDbConnection)new SqliteConnection(conn);
         dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
-
-        string sqlQueryQuestion = $"SELECT Questions.question_text, Answer_incorrect.answer_text_one, Answer_incorrect.answer_text_two, Answer_correct.answer_text FROM Questions,Answer_incorrect, Answer_correct  WHERE QID = {number} AND IID = {number} AND CID = {number};"; //WHERE QID =" + number; 
-        dbcmd.CommandText = sqlQueryQuestion;
-        IDataReader reader = dbcmd.ExecuteReader();
-            while (reader.Read())
-            {
-
-          string question = reader.GetString(0);
-            string inccorect_one = reader.GetString(1);
-            string inccorect_two = reader.GetString(2);
-            string correct = reader.GetString(3);
-            
-
-            Debug.Log(question);
-
-                Text quizText = GameObject.Find("Canvas/Panel/Quiz Spørgsmål").GetComponent<Text>();
-                quizText.text = question;
-            Text answerText1 = GameObject.Find("Canvas/Panel/Svar 1/Text").GetComponent<Text>();
-            answerText1.text = correct;
-            Button answer1 = GameObject.Find("Canvas/Panel/Svar 1").GetComponent<Button>();
-            answer1.onClick.AddListener(Answer1OnClick);
-            Text answerText2 = GameObject.Find("Canvas/Panel/Svar 2/Text").GetComponent<Text>();
-            answerText2.text = inccorect_one;
-            Button answer2 = GameObject.Find("Canvas/Panel/Svar 2").GetComponent<Button>();
-            answer2.onClick.AddListener(Answer2OnClick);
-            Text answerText3 = GameObject.Find("Canvas/Panel/Svar 3/Text").GetComponent<Text>();
-            answerText3.text = inccorect_two;
-            Button answer3 = GameObject.Find("Canvas/Panel/Svar 3").GetComponent<Button>();
-            answer3.onClick.AddListener(Answer3OnClick);
-
-
-        }
+        quizText = GameObject.Find("Canvas/Panel/Quiz Spørgsmål").GetComponent<Text>();
+        answerText1 = GameObject.Find("Canvas/Panel/Svar 1/Text").GetComponent<Text>();
+        answerText2 = GameObject.Find("Canvas/Panel/Svar 2/Text").GetComponent<Text>();
+        answerText3 = GameObject.Find("Canvas/Panel/Svar 3/Text").GetComponent<Text>();
+        answer1 = GameObject.Find("Canvas/Panel/Svar 1").GetComponent<Button>();
+        answer2 = GameObject.Find("Canvas/Panel/Svar 2").GetComponent<Button>();
+        answer3 = GameObject.Find("Canvas/Panel/Svar 3").GetComponent<Button>();
     }
 
     public void Answer1OnClick()
     {
         answer = true;
         Debug.Log("you answer is " + answer);
-        StartCoroutine(CD());
+        number = Random.Range(1, 10);
+        quiz.SetActive(false);
     }
     public void Answer2OnClick()
     {
         answer = false;
         Debug.Log("You answer is " + answer);
+        quiz.SetActive(false);
     }
     public void Answer3OnClick()
     {
         answer = false;
         Debug.Log("You answer is " + answer);
+        quiz.SetActive(false);
     }
 
     //Update is called once per frame
     void Update()
     {
-        if (answer == true)
+        dbcmd = dbconn.CreateCommand();
+        sqlQueryQuestion = $"SELECT Questions.question_text, Answer_incorrect.answer_text_one, Answer_incorrect.answer_text_two, Answer_correct.answer_text FROM Questions,Answer_incorrect, Answer_correct  WHERE QID = {number} AND IID = {number} AND CID = {number};"; //WHERE QID =" + number; 
+        dbcmd.CommandText = sqlQueryQuestion;
+        reader = dbcmd.ExecuteReader();
+        while (reader.Read())
         {
-            quiz.SetActive(false);
-            StartCoroutine(CD());
+
+            question = reader.GetString(0);
+            incorrect_one = reader.GetString(1);
+            incorrect_two = reader.GetString(2);
+            correct = reader.GetString(3);
+
+            Debug.Log(question + number);
+
+            quizText.text = question;
+            
+            answerText1.text = correct;
+            answer1.onClick.AddListener(Answer1OnClick);
+            answerText2.text = incorrect_one;
+            answer2.onClick.AddListener(Answer2OnClick);
+            answerText3.text = incorrect_two;
+            answer3.onClick.AddListener(Answer3OnClick);
+
+            foreach (string question in questionList)
+            {
+                
+            }
+
+            if (questionList.Count == 0)
+            {
+
+            }
+
+            //StartCoroutine(CD());
         }
+        //if (answer == true)
+        //{
+        //    quiz.SetActive(false);
+        //    //StartCoroutine(CD());
+        //}
         stallScript = stall.gameObject.GetComponent<Stall_Script>();
 
-        if (stallScript != null && quiz.activeSelf == true)
-        {
-            number = stallScript.number;
-        }
+        //if (stallScript != null && quiz.activeSelf == tru)
+        //{
+        //    number = stallScript.number;
+        //}
     }
 
-    IEnumerator CD()
-    {
-        yield return new WaitForSeconds(15f);
-        answer = false;
-    }
+    //IEnumerator CD()
+    //{
+    //    yield return new WaitForSeconds(15f);
+    //    answer = false;
+    //}
 }
---
+++ CityLabGame/Assets/Scripts/ProgressBarScriptBlue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarScriptBlue : MonoBehaviour
+{
+    public Slider slider;
+    public Text blue;
+
+    private float fillSpeed = 0.2f;
+    private float targetProgress = 0;
+
+    private int gridAmount = 4522;
+
+    public float SliderValue { get; private set; }
+
+    public void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        //IncrementProgressBar(0.75f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SliderValue = PlayerGridCollision.blueScore / 45.22f;
+        slider.value = SliderValue / 100;
+        blue.text = "" + slider.value + "%";
+    }
+
+    //public void IncrementProgressBar(float newProgress)
+    //{
+    //    targetProgress = slider.value + newProgress;
+    //}
+}
